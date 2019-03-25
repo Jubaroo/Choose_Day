f2db9688b1c2dd5a1813e06ee2b770fefcb9393f
Zeus <requiemofwurm@gmail.com>
2019-03-25 18:45:57 -0400

    Initial commit

---
+++ BetterBlueprints.cs
@@ -0,0 +1,94 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: BetterBlueprints.BetterBlueprintsCore
+// Assembly: BetterBlueprints, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 949B4F57-3651-4C23-B4C4-E713EBCAD46C
+// Assembly location: D:\SteamLibrary\steamapps\common\The Forest\TheForest_Data\Managed\BetterBlueprints.dll
+
+using ModAPI;
+using ModAPI.Attributes;
+using System;
+using System.IO;
+using TheForest.Buildings.Creation;
+using TheForest.Utils;
+using UnityEngine;
+
+namespace BetterBlueprints
+{
+    internal class BetterBlueprintsCore : MonoBehaviour
+    {
+        public static int MaxAnchorPoints = 10000;
+        private readonly Config _config = new Config("Mods/BetterBlueprints.settings", "BetterBlueprints");
+        public static Color TintColor;
+        public static bool BuildAnywhereToggle;
+        public static bool InfiniteZiplineToggle;
+        private const string ConfigPath = "Mods/BetterBlueprints.settings";
+
+        [ExecuteOnGameStart]
+        private static void AddMeToScene()
+        {
+            if (!Application.loadedLevelName.ToLower().Contains("forest"))
+                return;
+            new GameObject("__BetterBlueprints__").AddComponent<BetterBlueprintsCore>();
+        }
+
+        private void Awake()
+        {
+            this.ReadConfig();
+            this.InvokeRepeating("SetMaxAnchorpointsWall", 1f, 5f);
+            this.InvokeRepeating("ReadConfig", 1f, 2f);
+        }
+
+        private void Update()
+        {
+            LocalPlayer.Create.BuildingPlacer.ClearMat.SetColor("_TintColor", BetterBlueprintsCore.TintColor);
+        }
+
+        private void SetMaxAnchorpointsWall()
+        {
+            ((WallArchitect)UnityEngine.Object.FindObjectOfType(typeof(WallArchitect)))._maxPoints = BetterBlueprintsCore.MaxAnchorPoints;
+        }
+
+        private void CheckConfig()
+        {
+            if (File.Exists("Mods/BetterBlueprints.settings"))
+                return;
+            Log.Write("Configfile is missing", "BetterBlueprints");
+            this.CreateConfig();
+        }
+
+        private void CreateConfig()
+        {
+            Log.Write("Creating new default config...", "BetterBlueprints");
+            this._config.WriteInt("red", 0, "BlueprintColor");
+            this._config.WriteInt("green", 0, "");
+            this._config.WriteInt("blue", 100, "");
+            this._config.WriteInt("alpha", 25, "");
+            this._config.WriteBool("buildAnywhere", true, "Blueprints");
+            this._config.WriteBool("infiniteZipline", true, "Blueprints");
+        }
+
+        private void ReadConfig()
+        {
+            this.CheckConfig();
+            try
+            {
+                BetterBlueprintsCore.TintColor.r = this._config.ReadFloat("red", 0.0f, "BlueprintColor") / 100f;
+                BetterBlueprintsCore.TintColor.g = this._config.ReadFloat("green", 0.0f, "") / 100f;
+                BetterBlueprintsCore.TintColor.b = this._config.ReadFloat("blue", 100f, "") / 100f;
+                BetterBlueprintsCore.TintColor.a = this._config.ReadFloat("alpha", 25f, "") / 100f;
+                BetterBlueprintsCore.BuildAnywhereToggle = this._config.ReadBool("buildAnywhere", true, "Blueprints");
+                BetterBlueprintsCore.InfiniteZiplineToggle = this._config.ReadBool("infiniteZipline", true, "Blueprints");
+            }
+            catch (Exception ex)
+            {
+                Log.Write("Error while reading the config from: Mods/BetterBlueprints.settings", "BetterBlueprints");
+                Log.Write(ex.Message, "BetterBlueprints");
+                Log.Write(ex.StackTrace, "BetterBlueprints");
+                if (File.Exists("Mods/BetterBlueprints.settings"))
+                    File.Move("Mods/BetterBlueprints.settings", "Mods/BetterBlueprints.settings.old");
+                this.CreateConfig();
+                this.ReadConfig();
+            }
+        }
+    }
+}
---
+++ ChooseDay.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ModAPI.Attributes;
+using TheForest.Utils;
+using UnityEngine;
+
+namespace Choose_Day
+{
+    internal class ChooseDay : MonoBehaviour
+    {
+        [ExecuteOnGameStart]
+        private static void AddMeToScene()
+        {
+            new GameObject("__BetterBlueprints__").AddComponent<ChooseDay>();
+        }
+
+        private bool visible;
+        protected GUIStyle labelStyle;
+        private string day = "";
+
+        private void OnGUI()
+        {
+            if (visible)
+            {
+                // use ModAPI Skin
+                GUI.skin = ModAPI.Interface.Skin;
+
+                // apply label style if not existing
+                if (this.labelStyle == null)
+                {
+                    this.labelStyle = new GUIStyle(GUI.skin.label)
+                    {
+                        fontSize = 12
+                    };
+                }
+
+                // create box (background)
+                GUI.Box(new Rect(10f, 10f, 300f, 150f), "", GUI.skin.window);
+
+                // Label
+                GUI.Label(new Rect(30f, 15f, 200f, 20f), "Choose The Current Game Day", this.labelStyle);
+
+                // Text-input
+                day = GUI.TextField(new Rect(30f, 50f, 200f, 30f), day, GUI.skin.textField);
+
+                // Button
+                if (GUI.Button(new Rect(30f, 100f, 80f, 20f), "Set Day"))
+                {
+                    Clock.Day = Int32.Parse(day);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            // if clicked button
+            if (ModAPI.Input.GetButtonDown("Menu"))
+            {
+                // show cursor
+                if (visible)
+                {
+                    LocalPlayer.FpCharacter.UnLockView();
+                }
+                else
+                {
+                    LocalPlayer.FpCharacter.LockView(true);
+                }
+                // toggle menu
+                visible = !visible;
+            }
+        }
+    }
+}
